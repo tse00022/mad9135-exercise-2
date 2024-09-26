c9b3025f896320a006703165b40749b03a8ad62b
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-09-25 20:05:25 -0400

    Done exercise 2

--- bin/exercise2.dart
+++ bin/exercise2.dart
@@ -1,6 +1,8 @@
-import 'dart:io';
 import 'package:exercise2/exercise2.dart';
 
 void main(List<String> arguments) {
-  print(Lucky().number);
+  for (var arg in arguments) {
+    print(
+        'Hello ${arg[0].toUpperCase() + arg.substring(1)}. Your lucky number is ${Lucky().number}');
+  }
 }
